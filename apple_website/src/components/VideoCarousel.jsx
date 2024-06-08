e5c54a9e5f2fbd5784f701002721ab504e0f3efc
Shiwarne Silva <103165392+Shiwarne-Silva@users.noreply.github.com>
2024-06-08 18:57:24 +0530

    Update VideoCarousel.jsx

--- apple_website/src/components/VideoCarousel.jsx
+++ apple_website/src/components/VideoCarousel.jsx
@@ -1,11 +1,12 @@
 import gsap from "gsap";
 import { useGSAP } from "@gsap/react";
 import { ScrollTrigger } from "gsap/all";
-gsap.registerPlugin(ScrollTrigger);
 import { useEffect, useRef, useState } from "react";
 
 import { hightlightsSlides } from "../constants";
 import { pauseImg, playImg, replayImg } from "../utils";
+
+gsap.registerPlugin(ScrollTrigger);
 
 const VideoCarousel = () => {
   const videoRef = useRef([]);
